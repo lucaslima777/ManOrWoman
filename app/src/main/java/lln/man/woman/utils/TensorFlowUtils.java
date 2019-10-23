7c67139e5c17a65567266abfbcfe5afea1ffa515
Lucas Lima T <lukaslymauchiha@gmail.com>
2019-10-23 02:04:42 -0300

    Adicionado classe Util Tensorflow

---
+++ app/src/main/java/lln/man/woman/utils/TensorFlowUtils.java
@@ -0,0 +1,11 @@
+package lln.man.woman.utils;
+
+/**
+ * @author lucaslima
+ */
+public class TensorFlowUtils {
+
+    public final String MODEL_PATH = "optimized_graph.lite";
+    public final String LABEL_PATH = "retrained_labels.txt";
+    public final int INPUT_SIZE = 299;
+}
